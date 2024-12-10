dcc97bf6908e94a3c9f5f947d415916b72c725e2
Dalton Larsen <dalton.larsen22@gmail.com>
2024-12-10 16:58:02 -0700

    Added bios for each horse and javascript for each modal

--- src/mares.js
+++ src/mares.js
@@ -1,7 +1,26 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-//---------------KONO-----------------
+//---------------Coko-----------------
+var img = document.getElementById("coko");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+//---------------Kono-----------------
 var img = document.getElementById("kono");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -19,6 +38,7 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
 //---------------Pendelton-----------------
 var img = document.getElementById("pendelton");
 var modalImg = document.getElementById("img01");
@@ -37,38 +57,3 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-//---------------Konos Gone Rogue-----------------
-var img = document.getElementById("konosGoneRogue");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-//---------------Coko Pebbles-----------------
-var img = document.getElementById("cokoPebbles");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
--- src/racehorses.js
+++ src/racehorses.js
@@ -1,8 +1,8 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-//---------------KONO-----------------
-var img = document.getElementById("kono");
+//---------------CB Docs Bella Terra-----------------
+var img = document.getElementById("cbDocsBellaTerra");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
@@ -19,25 +19,8 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-//---------------Pendelton-----------------
-var img = document.getElementById("pendelton");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-//---------------Konos Gone Rogue-----------------
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+//---------------CB Docs Bella Terra-----------------
 var img = document.getElementById("konosGoneRogue");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -55,8 +38,47 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-//---------------Coko Pebbles-----------------
-var img = document.getElementById("cokoPebbles");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+//---------------Slam Diego-----------------
+var img = document.getElementById("slamDiego");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+//---------------WTD Italian Princess-----------------
+var img = document.getElementById("wtdItalianPrincess");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+//---------------WTD Not Interested-----------------
+var img = document.getElementById("wtdNotInterested");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
